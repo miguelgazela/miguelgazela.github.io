b6d4cb89aff156e3c45750b70af75b397a061b6d
Miguel Oliveira <miguel.gazela@gmail.com>
2015-03-09 16:43:38 +0000

    Added first gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,61 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	cache = require('gulp-cache'),
+	livereload = require('gulp-livereload'),
+	del = require('del');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('styles', function () {
+	return sass('css/main.scss.haml', { style: 'expanded' })
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(rename({ suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function () {
+	return gulp.src('js/*.js')
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'))
+		.pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function () {
+	return gulp.src('img/**/*')
+		.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('dist/img'))
+		.pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('watch', function () {
+	// watch .scss files
+	gulp.watch('css/.scss', ['styles']);
+
+	// watch .js files
+	gulp.watch('js/*.js', ['scripts']);
+
+	// watch image files
+	gulp.watch('img/**/*', ['images']);
+});
+
+gulp.task('clean', function (cb) {
+	del(['dist/css', 'dist/js', 'dist/img'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('styles', 'scripts', 'images');
 });
